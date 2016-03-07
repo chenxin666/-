272c4b64fafc48de9ae581e218078ffefbc30839
chenxin666 <1849873003@qq.com>
2016-03-07 21:42:12 +0800

    Rename ArraySum.cpp to prog 2.cpp

--- prog 2.cpp
+++ prog 2.cpp
@@ -6,16 +6,16 @@ int ArraySum(int n)
 {
 	int *a,sum=0,i;
     a=new int[n];
-	ifstream in("E:\\数据来源1.txt");
+	ifstream in("E:\\鏁版嵁鏉ユ簮1.txt");
 	for(i=0;in>>a[i],i<n;i++)
 	{   sum+=a[i];}
-	cout<<"元素的和为："<<sum<<endl;
+	cout<<"鍏冪礌鐨勫拰涓猴細"<<sum<<endl;
 	return sum;}
 
 int main()
 {
 	int n=0;
-	cout<<"输入任意数组长度:";
+	cout<<"杈撳叆浠绘剰鏁扮粍闀垮害:";
 	cin>>n;
 	ArraySum(n);
 	return 0;
